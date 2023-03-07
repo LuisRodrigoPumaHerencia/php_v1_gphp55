ebae6c068cec0b28a7370dbc2c15b44ebb62c86f
Luis Rodrigo Puma Herencia <U19314535@utp.edu.pe>
2023-03-06 21:56:12 -0500

    listado docentes

--- view/docentes/docentes.php
+++ view/docentes/docentes.php
@@ -1,7 +1,11 @@
 <?php
     include '../layout/header.php';
+    include '../../model/conexion.php';
 ?>
-
+<?php
+    $sentencia = $con -> query("SELECT * FROM docentes");
+    $docentes = $sentencia -> fetchAll(PDO::FETCH_OBJ);
+?>
 <div class="container text-center">
     <h1>LISTA DOCENTES</h1>
     <div class="row justify-items-center mb-3">
@@ -17,16 +21,22 @@
             </tr>
         </thead>
         <tbody class="table table-dark">
+            <?php
+            foreach($docentes as $docente){
+            ?>
             <tr>
-                <td>1</td>
-                <td>JUAN MIGUEL</td>
-                <td>PEREZ RAMIREZ</td>
-                <td>24</td>
+                <td><?php echo $docente->id_docente ?></td>
+                <td><?php echo $docente->primer_nombre ?> <?php echo $docente->segundo_nombre ?></td>
+                <td><?php echo $docente->primer_apellido ?> <?php echo $docente->segundo_apellido ?></td>
+                <td><?php echo $docente->edad ?></td>
                 <td>
                     <a href="" type="button" class="btn btn-primary"><i class="fa-sharp fa-solid fa-pen"></i></a>
                     <a href="" type="button" class="btn btn-danger"><i class="fa-sharp fa-solid fa-trash"></i></a>
                 </td>
             </tr>
+            <?php
+            }
+            ?>
         </tbody>
     </table>
 </div>
